fb7c4e0e1eebcedeff263ebe5e15a02c45101c80
agata-gacevic <agata.gacevic@gmail.com>
2018-06-10 18:41:13 +0200

    6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,45 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
-    public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args){
+
+        //test User
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result1 = simpleUser.getUsername();
+
+        if (result1.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        //test Calculator add
+        System.out.println("\nTest Calculator - addition\n");
+
+        Calculator calculator = new Calculator();
+
+        Integer result2 = calculator.add(50, 25);
+
+        if (result2.equals(75)) {
+            System.out.println("test passed!");
+        } else {
+            System.out.println("Error!");
+        }
+
+        //test Calculator substract
+        System.out.println("\nTest Calculator - substraction\n");
+
+        Integer result3 = calculator.substract(50, 40);
+
+        if (result3.equals(25)) {
+            System.out.println("test passed!");
+        } else {
+            System.out.println("Error!");
+        }
+
     }
 }
